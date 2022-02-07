c0cc48e186df43a2048a640a6e0e439f876968d6
Abramov Iurii <abtank@outlook.com>
2022-02-08 02:50:02 +0300

    SH-19 Настройка почты для рассылок

--- oapi/src/main/java/ru/team/scheduler/oapi/OapiApplication.java
+++ oapi/src/main/java/ru/team/scheduler/oapi/OapiApplication.java
@@ -1,10 +1,23 @@
 package ru.team.scheduler.oapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.team.scheduler.oapi.services.EmailService;
+
+import javax.mail.MessagingException;
 
 @SpringBootApplication
 public class OapiApplication {
+
+//    Пример рассылки
+    private EmailService emailService;
+    @Autowired
+    public void setEmailService(EmailService emailService) throws MessagingException {
+        this.emailService = emailService;
+        emailService.sendSimpleMessage(new String[]{"spprtscheduler@gmail.com","scheduler@internet.ru"},"Заголовок","Какой-то там текст.");
+//        emailService.sendMessageWithAttachment(new String[]{"spprtscheduler@gmail.com","scheduler@internet.ru"},"Заголовок с файлом","Какой-то там текст.","oapi/src/main/resources/application.yaml");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OapiApplication.class, args);
