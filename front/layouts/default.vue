566640ec5dd71fe6a1a25f0427dcfe5a95364efe
Abramov Iurii <abtank@outlook.com>
2022-02-10 02:53:43 +0300

    Фронт из SH-10

---
+++ front/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header />
+    <div class="content">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "~/components/Header";
+export default {
+  components: { Header },
+  name: "default"
+}
+</script>
+
+<style scoped lang="scss">
+.content {
+  padding: 10px 0;
+}
+</style>
