47be07f7304ca3aaae98806c1d1620255ae0cfca
Abramov Iurii <abtank@outlook.com>
2022-03-05 05:07:03 +0300

    baseURL nuxt.config.js

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8189/sh/api/v1/',
+    baseURL: 'https://scheduler-team.herokuapp.com/sh/api/v1/',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
